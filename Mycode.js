03dfbaab920fb9ae9ad3ee447adc85d78944ed9c
tharani-21 <82307476+tharani-21@users.noreply.github.com>
2021-05-24 10:01:23 +0530

    Add files via upload

---
+++ Mycode.js
@@ -0,0 +1,16 @@
+function login()
+{
+    location.href="login.html";
+}
+function home()
+{
+    location.href="My code.html";
+}
+function contact()
+{
+    location.href="contact.html";
+}
+function about()
+{
+    location.href="about.html";
+}
